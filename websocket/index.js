e8cc773d32da1b3cdc40b68c20ef63c2f9d7a0fd
rohit28092003 <rohitdeepchandani@gmail.com>
2025-02-15 22:57:19 +0530

    f

--- websocket/index.js
+++ websocket/index.js
@@ -20,7 +20,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://chatbot-smar-5m8qev9q6-rohit28092003s-projects.vercel.app/", credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection
